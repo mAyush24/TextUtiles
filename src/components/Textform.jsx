5cd71d523c502587948849c0eb388f5de6f172ca
mAyush24 <ayush2524mishra@gmail.com>
2023-08-23 12:44:30 +0530

    fix some bugs

--- src/components/Textform.jsx
+++ src/components/Textform.jsx
@@ -16,11 +16,11 @@ export default function Textform(props) {
         setnewText(event.target.value)
     }
     const upCase = () => {
-        let upText = text.toUpperCase();
+        let upText = newText.toUpperCase();
         setnewText(upText)
     }
     const lwCase = () => {
-        let lwText = text.toLowerCase();
+        let lwText = newText.toLowerCase();
         setnewText(lwText)
     }
     const clear = () => {
@@ -41,14 +41,14 @@ export default function Textform(props) {
         setnewText(newText.trim())
     }
     const extraSpaces = () => {
-        let spaces = text.split(/[ ]+/)
+        let spaces = newText.split(/[ ]+/)
         setnewText(spaces.join(" "))
     }
 
 
     useEffect(() => {
         // array of words
-        const words = text.split(' ');
+        const words = text.split(/\s+/);
 
         // update word count
         let wordCount = 0;
@@ -64,19 +64,19 @@ export default function Textform(props) {
     return (
         <div className='container flex my-3'>
             <div className="mx-1" style={width}>
-                <h1>{props.title}</h1>
+                <h2>{props.title}</h2>
                 <div className="mb-3">
                     <textarea placeholder='Enter some text' value={text} onChange={onChange} className="form-control" id="exampleFormControlTextarea1" rows="25"></textarea>
                 </div>
-                <button className="btn btn-primary mx-1 my-2" onClick={upCase}>Convert to UpperCase</button>
-                <button className="btn btn-primary mx-1 my-2" onClick={lwCase}>Convert to LowerCase</button>
+                <button className="btn btn-primary mx-1 my-2" onClick={upCase}>UpperCase</button>
+                <button className="btn btn-primary mx-1 my-2" onClick={lwCase}>LowerCase</button>
                 <button className="btn btn-primary mx-1 my-2" onClick={extraSpaces}>Remove Extra Spaces</button>
                 <button className="btn btn-primary mx-1 my-2" onClick={trim}>Trim Spaces</button>
                 <button className="btn btn-primary mx-1 my-2" onClick={clear}>Clear</button>
                 <button className="btn btn-primary mx-1 my-2" onClick={copy}>Copy</button>
             </div>
             <div className="mx-1" style={width}>
-                <h1 className='mx-3'>Preview Your Text</h1>
+                <h2>Preview Your Text...</h2>
                 <div className='mb-3'>
                     <textarea disabled className='form-control' id="exampleFormControlTextarea1" rows="25" value={newText.length > 0 ? newText : "Enter above to preview it here"}></textarea>
                 </div>
